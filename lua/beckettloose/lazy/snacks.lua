6333f59d81d32f3dee3de1d32be9f666a409f4ab
Beckett Loose <beckettloose@me.com>
2025-07-24 14:52:23 -0400

    Add custom snacks.nvim file type switcher

--- lua/beckettloose/lazy/snacks.lua
+++ lua/beckettloose/lazy/snacks.lua
@@ -1,17 +1,44 @@
 return {
-	"folke/snacks.nvim",
-	opts = {
-		picker = {},
-		explorer = {},
-	},
-	keys = {
-		{ "<leader><space>", function () Snacks.picker.smart() end, desc = "Smart Find Files" },
-		{ "<leader>,", function () Snacks.picker.buffers() end, desc = "Smart Find Buffers" },
-		{ "<C-p>", function () Snacks.picker.git_files() end, desc = "Find Git Files" },
-		{ "<leader>/", function () Snacks.picker.lines() end, desc = "Lines" },
-		{ "<leader>vh", function () Snacks.picker.help() end, desc = "[H]elp Search" },
-		{ "<leader>vk", function () Snacks.picker.keymaps() end, desc = "[K]eymap Search" },
-		{ "<leader>x", function () Snacks.picker.explorer() end, desc = "E[x]plorer" },
-		-- Still need <leader>vcl to open filetype switcher prompt
-	}
+    "folke/snacks.nvim",
+    opts = {
+        picker = {
+            sources = {
+                filetypes = {
+                    name = "filetypes",
+                    format = "text",
+                    preview = "none",
+                    layout = { preset = "vscode"},
+                    confirm = function(picker, item)
+                        picker:close()
+                        if item then
+                            vim.schedule(function()
+                                vim.cmd("setfiletype " .. item.text)
+                            end)
+                    end
+                    end,
+                    finder = function()
+                      local items = {}
+                      local filetypes = vim.fn.getcompletion("", "filetype")
+                      for _, type in ipairs(filetypes) do
+                        items[#items + 1] = {
+                          text = type,
+                        }
+                      end
+                      return items
+                    end,
+                },
+            },
+		},
+        explorer = {},
+    },
+    keys = {
+        { "<leader><space>", function () Snacks.picker.smart() end, desc = "Smart Find Files" },
+        { "<leader>,", function () Snacks.picker.buffers() end, desc = "Smart Find Buffers" },
+        { "<C-p>", function () Snacks.picker.git_files() end, desc = "Find Git Files" },
+        { "<leader>/", function () Snacks.picker.lines() end, desc = "Lines" },
+        { "<leader>vh", function () Snacks.picker.help() end, desc = "[H]elp Search" },
+        { "<leader>vk", function () Snacks.picker.keymaps() end, desc = "[K]eymap Search" },
+        { "<leader>x", function () Snacks.picker.explorer() end, desc = "E[x]plorer" },
+        { "<leader>vcl", function () Snacks.picker.filetypes() end, desc = "[L]anguage" },
+    }
 }
