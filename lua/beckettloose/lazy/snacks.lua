e8f9fb0f69335c6f7effac3dab8c4b18b7882457
Beckett Loose <beckettloose@me.com>
2025-07-31 11:17:41 -0400

    Fix warning in snacks config

--- lua/beckettloose/lazy/snacks.lua
+++ lua/beckettloose/lazy/snacks.lua
@@ -7,7 +7,7 @@ return {
         Snacks.toggle.option("list", { name = "󱁐 List Whitespace" }):map("<leader>vtl")
         Snacks.toggle.option("expandtab", { name = "󰡎 Expand Tab" }):map("<leader>vte")
         Snacks.toggle.treesitter({ name = " Treesitter" }):map("<leader>vtt")
-        Snacks.toggle.indent({ name = "│ Indent" }):map("<leader>vti")
+        Snacks.toggle.indent():map("<leader>vti")
 
         return {
             bigfile = {},
