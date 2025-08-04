1e1ee93cf75f60d1c84d515d01a500fab38d8082
Beckett Loose <beckettloose@me.com>
2025-08-04 09:27:18 -0400

    Only load cloak on .env files

--- lua/beckettloose/lazy/cloak.lua
+++ lua/beckettloose/lazy/cloak.lua
@@ -1,6 +1,6 @@
 return {
     "laytan/cloak.nvim",
-    event = "VimEnter",
+    event = "BufEnter .env*",
     config = function()
         require("cloak").setup({
             enabled = true,
@@ -13,8 +13,6 @@ return {
                     -- This can be a table to match multiple file patterns.
                     file_pattern = {
                         ".env*",
-                        "wrangler.toml",
-                        ".dev.vars",
                     },
                     -- Match an equals sign and any character after it.
                     -- This can also be a table of patterns to cloak,
