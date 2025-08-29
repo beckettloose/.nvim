132286fb28dc721ff0279b4f52d9dc5456fd6120
Beckett Loose <beckettloose@me.com>
2025-08-29 09:28:36 -0400

    First attempt at switching to nvim-treesitter main branch

--- lua/beckettloose/lazy/treesitter.lua
+++ lua/beckettloose/lazy/treesitter.lua
@@ -1,27 +1,33 @@
 return {
     "nvim-treesitter/nvim-treesitter",
-    event = "VimEnter",
+    branch = "main",
+    lazy = false,
     build = ":TSUpdate",
     opts = {
-        ensure_installed = { "bash", "c", "diff", "html", "lua", "luadoc", "markdown", "vim", "vimdoc" },
+        -- Directory to install parsers and queries to
+        install_dir = vim.fn.stdpath('data') .. '/site',
+        -- ensure_installed = { "bash", "c", "diff", "html", "lua", "luadoc", "markdown", "vim", "vimdoc" },
         -- Autoinstall languages that are not installed
-        auto_install = true,
-        highlight = {
-            enable = true,
+        -- auto_install = true,
+        -- highlight = {
+            -- enable = true,
             -- Some languages depend on vim's regex highlighting system (such as Ruby) for indent rules.
             --  If you are experiencing weird indenting issues, add the language to
             --  the list of additional_vim_regex_highlighting and disabled languages for indent.
-            additional_vim_regex_highlighting = { "ruby" },
-        },
-        indent = { enable = true, disable = { "ruby" } },
+            -- additional_vim_regex_highlighting = { "ruby" },
+        -- },
+        -- indent = { enable = true, disable = { "ruby" } },
     },
     config = function(_, opts)
         -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
 
         -- Prefer git instead of curl in order to improve connectivity in some environments
-        require("nvim-treesitter.install").prefer_git = true
+        -- require("nvim-treesitter.install").prefer_git = true
         ---@diagnostic disable-next-line: missing-fields
-        require("nvim-treesitter.configs").setup(opts)
+        require("nvim-treesitter").setup(opts)
+        require("nvim-treesitter").install({
+            "bash", "c", "diff", "html", "lua", "luadoc", "markdown", "vim", "vimdoc"
+        })
 
         -- There are additional nvim-treesitter modules that you can use to interact
         -- with nvim-treesitter. You should go explore a few and see what interests you:
