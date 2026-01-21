62cf06cc394c5461376bf08639f3ff0723b8de1c
Beckett Loose <beckettloose@me.com>
2026-01-21 15:18:05 -0500

    feat: add basic lsp keybinds for rename and code actions

--- lua/beckettloose/lazy/lsp.lua
+++ lua/beckettloose/lazy/lsp.lua
@@ -12,5 +12,18 @@ return {
             "cmake_language_server",
             "jdtls"
         })
+
+        vim.api.nvim_create_autocmd('LspAttach', {
+            group = vim.api.nvim_create_augroup('beckettloose-lsp-attach', { clear = true }),
+            callback = function (event)
+                local map = function (keys, func, desc, mode)
+                    mode = mode or 'n'
+                    vim.keymap.set(mode, keys, func, { buffer = event.buf, desc = 'LSP: ' .. desc })
+                end
+
+                map('<leader>cr', vim.lsp.buf.rename, '[r]ename')
+                map('<leader>ca', vim.lsp.buf.code_action, '[a]ctions')
+            end
+        })
     end,
 }
