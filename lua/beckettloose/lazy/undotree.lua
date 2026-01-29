0d5b4cd9c1eeb3c0e9638ccccd5890f3944f8dda
Beckett Loose <beckettloose@me.com>
2026-01-29 07:44:16 -0500

    feat: add keybind to focus undotree window

--- lua/beckettloose/lazy/undotree.lua
+++ lua/beckettloose/lazy/undotree.lua
@@ -2,6 +2,7 @@ return {
     "mbbill/undotree",
     event = "VimEnter",
     config = function()
-        vim.keymap.set("n", "<leader>u", vim.cmd.UndotreeToggle, { desc = "Toggle [U]ndo Tree" })
+        vim.keymap.set("n", "<leader>u", vim.cmd.UndotreeToggle, { desc = "Toggle [u]ndo Tree" })
+        vim.keymap.set("n", "<leader>U", vim.cmd.UndotreeFocus, { desc = "Focus [U]ndo Tree" })
     end,
 }
