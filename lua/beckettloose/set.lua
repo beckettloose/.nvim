5149e88905cada22b1f3124e6851ac6f7beff24a
Beckett Loose <beckettloose@me.com>
2026-02-09 14:07:15 -0500

    feat!: move nvim undo directory to proper location
    To fix breaking change: Move ~/.vim/undodir to
    ~/.local/share/nvim/undodir

--- lua/beckettloose/set.lua
+++ lua/beckettloose/set.lua
@@ -28,7 +28,7 @@ vim.opt.breakindent = true
 -- Disable swap and backup files, enable undofile
 vim.opt.swapfile = false
 vim.opt.backup = false
-vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
+vim.opt.undodir = os.getenv("HOME") .. "/.local/share/nvim/undodir"
 vim.opt.undofile = true
 
 -- Disable mouse mode
